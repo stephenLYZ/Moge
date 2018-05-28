f03806b5318a1e282031d2cda894b23e4b2911b9
huaoguo <huaoguo@gmail.com>
2018-05-28 15:44:56 +0800

    refactor: 将检测碰撞移动到 thing 基类

--- src/thing/index.js
+++ src/thing/index.js
@@ -27,11 +27,62 @@ export default class Thing {
     this.position = position
   }
 
-  update (time) {
-    this.move(time)
-  }
-
   get moveable () {
     return !!this.mover
+  }
+
+  update (time) {
+    this.move(time)
+    this.checkEdge()
+    this.checkCollide()
+  }
+
+  checkCollide () {
+    let collied = false
+
+    let { collideThing, direction } = this._scene.collideX(this)
+    if (collideThing) {
+      switch(direction) {
+        case 'right':
+        case 'left':
+          this.position.x = this.prevPosition.x
+          collied = true
+          break
+      }     
+    }
+
+    let collideY = this._scene.collideY(this)
+    if (collideY.collideThing) {
+      direction = collideY.direction
+      switch(collideY.direction) {
+        case 'down':
+          this.position.y = this.prevPosition.y
+          collied = true
+          break
+      }     
+    }
+
+    if (collied) {
+      this._scene.emit('collide', { which: direction, target: this })
+    }
+  }
+
+  checkEdge () {
+    // 检测碰撞
+    const { position: { x, y}, shape: { width, height } } = this
+    if (y + height > this._scene.height) {
+      this.position.y = this._scene.height - height
+    }
+    if (x + width > this._scene.width) {
+      this.position.x = this._scene.width - width
+    } else if (x < 0) {
+      this.position.x = 0
+    }
+    if (!this.reachEdge) {
+      if (this.position.y + height === this._scene.height) {
+        this._scene.emit('collide', { which: 'down', target: this })
+        this.reachEdge = true
+      }
+    }
   }
 }
